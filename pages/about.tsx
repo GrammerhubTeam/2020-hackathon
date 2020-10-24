ee1610acf316dd3fb9ed9615ed1757299158cf04
Kyle Williams <kwilliams@peakactivity.com>
2020-10-24 12:27:09 -0400

    fixed material bug

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import Layout from "../components/MyLayout";
-import AboutUs from "../components/Home/AboutUs/AboutUs";
+// import AboutUs from "../components/Home/AboutUs/AboutUs";
+// import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Button, IconButton, /*makeStyles,*/ Toolbar, Typography } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+// }));
 
 
-export default () => (
+export default () => {
+  // const classes = useStyles();
+  return (
   <Layout>
-    < AboutUs />
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" /*className={classes.menuButton}*/ color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" /*className={classes.title}*/>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   </Layout>
-);
+)};
