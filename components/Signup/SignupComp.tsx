bc053e045de06d637cea4d346312fe17a7f6fd38
Donald Murillo <donald.murillo07@gmail.com>
2020-10-24 21:11:46 -0400

    - made second page and styles

--- components/Signup/SignupComp.tsx
+++ components/Signup/SignupComp.tsx
@@ -35,52 +35,90 @@ const SignupComp = () => {
 
     return (
         // <Layout>
+        
             <>
-                <p className={`${css.center} ${css.logo}`}>Grammerhood</p>
-                <p className={css.center}>Sign up to play interactive scavenger hunt throughout your neighborhood</p>
-                <p className={`${css.center} ${css.started}`}><strong>Lets get started!</strong></p>
-                <form noValidate autoComplete="off">
-                    <div className={css.divStyle}>
-                        <TextField id="outlined-basic" label="Name" variant="outlined" />
-                    </div>
-                    <div className={css.divStyle}>
-                        <TextField id="outlined-basic1" label="Email" variant="outlined" />
-                    </div >
-                    <div className={css.divStyle}>
-                        {/* <TextField label="Age" variant="outlined" /> */}
-                        <FormControl variant="outlined" style={{  width: 220, color: "red" }} /*className={classes.formControl}*/>
-                            <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
-                            <Select
-                                native
-                                value={state.age}
-                                onChange={handleChange}
-                                label="Age"
-                                inputProps={{
-                                    name: 'age',
-                                    id: 'outlined-age-native-simple',
-                                }}
-                            >
-                            <option aria-label="None" value="" />
-                            
-                            <option value={10}>Ten</option>
-                            <option value={20}>Twenty</option>
-                            <option value={30}>Thirty</option>
-                            </Select>
-                        </FormControl>
-                    </div>
-                    <div className={css.divStyle}>
-                        <TextField label="Phone" variant="outlined" />
-                    </div>
-                    <div className={css.divStyle}>
-                        {/* <TextField label="Outlined" variant="outlined" /> */}
-                        <button className={css.button} >Next</button>
-                    </div>
-                    {/* <div style={divStyle}>
-                        <TextField label="Outlined" variant="outlined" />
-                    </div> */}
-                </form>
+                {activeStep === 0 ? 
+                <>  
+                    <p className={`${css.center} ${css.logo}`}>Grammerhood</p>
+                    <p className={css.center}>Sign up to play interactive scavenger hunt throughout your neighborhood</p>
+                    <p className={`${css.center} ${css.started}`}><strong>Lets get started!</strong></p>
+                    <form noValidate autoComplete="off">
+                        <div className={css.divStyle}>
+                            <TextField id="outlined-basic" label="Name" variant="outlined" />
+                        </div>
+                        <div className={css.divStyle}>
+                            <TextField id="outlined-basic1" label="Email" variant="outlined" />
+                        </div >
+                        <div className={css.divStyle}>
+                            {/* <TextField label="Age" variant="outlined" /> */}
+                            <FormControl variant="outlined" style={{  width: 220, color: "red" }} /*className={classes.formControl}*/>
+                                <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+                                <Select
+                                    native
+                                    value={state.age}
+                                    onChange={handleChange}
+                                    label="Age"
+                                    inputProps={{
+                                        name: 'age',
+                                        id: 'outlined-age-native-simple',
+                                    }}
+                                >
+                                <option aria-label="None" value="" />
+                                
+                                <option value={10}>10</option>
+                                <option value={20}>20</option>
+                                <option value={30}>Nonya</option>
+                                </Select>
+                            </FormControl>
+                        </div>
+                        <div className={css.divStyle}>
+                            <TextField label="Phone" variant="outlined" />
+                        </div>
+                        <div className={css.divStyle}>
+                            {/* <TextField label="Outlined" variant="outlined" /> */}
+                            <button className={css.button} onClick={handleNext}>Next</button>
+                        </div>
+                        {/* <div style={divStyle}>
+                            <TextField label="Outlined" variant="outlined" />
+                        </div> */}
+                    </form>
 
-                <div className={css.footer}>Already have an account? <strong className={css.orange}>Sign up</strong></div>
+                    <div className={css.footer}>Already have an account? <strong className={css.orange}>Sign up</strong></div>
+                </> :
+                <>
+                    <div onClick={handleBack}><span className="material-icons">keyboard_arrow_left</span> <span className={css.purple}>Back</span></div>
+
+                    <p className={`${css.center} ${css.started}`}><strong>Welcome to</strong></p>
+                    <p className={`${css.center} ${css.logo}`}>Grammerhood</p>
+                    <p className={`${css.center} ${css.started}`}><strong>One more step</strong></p>
+                    <p className={css.center}>PLease upload a picture of yourself</p>
+                    <p className={css.center}>We want to see your amazing costumes</p>
+                    <form noValidate autoComplete="off">
+                        <div className={css.picDiv}>
+                            <div className={css.item}>
+                                <span className={css.tex}>Take a Photo or Video</span>
+                                <span className={`material-icons md-light ${css.matI}`}>camera_alt</span>
+                            </div>
+                            <div className={css.itemMid}>
+                                <span className={css.tex}>Photo Library</span>
+                                <i className={`material-icons ${css.matI}`}>content_copy</i>
+                            </div>
+                            <div className={css.item}>
+                                <span className={css.tex}>Browse</span>
+                                <i className={`material-icons ${css.matI}`}>more_horiz</i>
+                            </div>
+                        </div>
+
+                        <div className={css.divStyle}>
+                            <button className={css.cancelButton} onClick={handleNext}>Cancel</button>
+                        </div>
+
+                    </form>
+
+                    
+                </>
+                }
+
 
                 {/* <MobileStepper
                     steps={maxSteps}
